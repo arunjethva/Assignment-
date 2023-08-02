a6d1385aea131386cb5fdd98f0be49bbbec6802b
arunjethva <141201433+arunjethva@users.noreply.github.com>
2023-08-02 09:04:57 +0530

    Add files via upload

---
+++ modual 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+main ()
+
+{
+
+printf("name - arun\n ");
+printf("age  - 20\n" );
+printf("bod  -  19 8 2003\n ");
+printf("address - dwarkhadhis society kosad Surat ");
+
+
+getch();
+
+}
